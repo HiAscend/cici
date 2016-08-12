c184075fc1c5cda6e3ea3180fc6f3d549ee8adc2
HiAscend <poseidon1237@163.com>
2016-08-12 11:01:05 +0800

    test

---
+++ test/src/main/java/com/zzia/Client.java
@@ -0,0 +1,11 @@
+package com.zzia;
+
+/**
+ * 描述：
+ * Created by ascend on 2016/8/12.
+ */
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
